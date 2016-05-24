592d26bab0070c3bcb96e4591c80c1d12bc43803
Andrew Su <asu@pivotal.io>
2016-05-24 14:19:49 -0400

    add PUT endpoint for config server
    [#119700171]
    https://www.pivotaltracker.com/story/show/119700171
    
    Signed-off-by: Vikram Yadav <vyadav@pivotal.io>

---
+++ README.md
@@ -0,0 +1,14 @@
+# Config Server
+
+Endpoints:
+
+
+GET /v1/config?key={key}
+---
+
+
+PUT /v1/config
+---
+Body:
+
+    key=<key>, value=<value>
--- src/config_server/main.go
+++ src/config_server/main.go
@@ -1,16 +1,40 @@
 package main
 
 import (
+	"os"
 	"io"
 	"net/http"
 )
 
-func dummy(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w,"dummy")
+var handler ConfigHandler
+
+type ConfigHandler struct {
+  db map[string]string
+}
+
+func (c ConfigHandler) handle(res http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	   case "GET":
+	   	   key := req.FormValue("key")
+	   	   result := c.db[key]
+	       io.WriteString(res, result)
+	   case "PUT":
+	       key := req.FormValue("key")
+	       value := req.FormValue("value")
+	       c.db[key] = value
+	   default:
+	       res.WriteHeader(http.StatusNotFound)
+	}
+}
+
+func config(w http.ResponseWriter, r *http.Request) {
+	handler.handle(w, r)
 }
 
 func main() {
-	http.HandleFunc("/v1/config/dummy", dummy)
-	http.ListenAndServe(":8000", nil)
-}
+	port := os.Args[1]
+	handler = ConfigHandler{ make(map[string]string) }
 
+	http.HandleFunc("/v1/config", config)
+	http.ListenAndServe(":" + port, nil)
+}
