a37d28f834a2dc87eaaba519e8a6c27320b80de7
Sukhil Suresh <ssuresh@pivotal.io>
2016-10-21 15:04:42 -0400

    Update test-integration.sh
    - Add default to run all db tests
    
    [#132713373](https://www.pivotaltracker.com/story/show/132713373)
    
    Signed-off-by: Sameer Vohra <svohra@pivotal.io>

--- scripts/test-integration.sh
+++ scripts/test-integration.sh
@@ -2,32 +2,54 @@
 
 set -e -x
 
-if [ -z "$1" ]; then
- echo "usage test-integration.sh [memory|postgresql|mysql]"
- exit 1
-fi
+setup_test_dir (){
+  rm -rf ./tmp
+  mkdir ./tmp
 
-export DB=$1
+  pushd ./tmp
 
-rm -rf ./tmp
-mkdir ./tmp
+  cp ../scripts/start_server.sh .
+  cp ../scripts/stop_server.sh .
+  cp ../scripts/setup_db.sh .
+  cp ../scripts/test-integration.sh .
+  chmod +x ./*
 
-pushd ./tmp
+  cp -r ../src/integration_tests/assets ./assets
 
-cp ../scripts/start_server.sh .
-cp ../scripts/stop_server.sh .
-cp ../scripts/setup_db.sh .
-cp ../scripts/test-integration.sh .
-chmod +x ./*
+  go build -o config_server.bin ../src/config_server/main.go
+  ginkgo build -r ../src/integration_tests
 
-cp -r ../src/integration_tests/assets ./assets
+  mv ../src/integration_tests/integration_tests.test .
+}
 
-#Copy DB file to config.json
-cp ./assets/config.$DB.json ./assets/config.json
+run_tests (){
+  export DB=$1
+  echo -e "\n\nRunning Integration tests for $DB\n\n"
+  #Copy DB file to config.json
+  cp ./assets/config.$DB.json ./assets/config.json
 
-go build -o config_server.bin ../src/config_server/main.go
-ginkgo build -r ../src/integration_tests
+  ./integration_tests.test
+}
 
-mv ../src/integration_tests/integration_tests.test .
+setup_test_dir
 
-./integration_tests.test
+case $1 in
+    memory  )
+     run_tests memory ;;
+    mysql  )
+     run_tests mysql ;;
+    postgresql  )
+     run_tests postgresql ;;
+    * )
+     if [ -n "$1" ]; then
+        echo "usage test-integration.sh [memory|postgresql|mysql] Defaults to all"
+        exit 1
+     fi
+     run_tests memory
+     run_tests mysql
+     run_tests postgresql
+     ;;
+esac
+
+
+
