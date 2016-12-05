98b14fd1ebd3b6b27e43a02ce938ab242e1563f2
Andrew Su <asu@pivotal.io>
2016-12-05 11:07:37 -0500

    Update ci/bosh-release to reflect new gocli changes
    [#134740685](https://www.pivotaltracker.com/story/show/134740685)
    
    Signed-off-by: Sameer Vohra <svohra@pivotal.io>

--- ci/tasks/bosh-release.sh
+++ ci/tasks/bosh-release.sh
@@ -4,4 +4,4 @@ set -e -x
 install bosh-cli/bosh-cli* /usr/local/bin/bosh
 
 cd config-server
-bosh create-release --force --tarball --name config-server --version acceptance
+bosh create-release --force --tarball=./config-server-release.tgz --name config-server --version acceptance
