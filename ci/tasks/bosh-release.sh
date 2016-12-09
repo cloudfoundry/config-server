94dd9a8a4cb2c9d8481e8e0e842edfa5c492e940
Dale Wick <dwick@pivotal.io>
2016-12-09 10:56:26 -0500

    Add bump & build job to pipeline
    [#135649083](https://www.pivotaltracker.com/story/show/135649083)
    
    Signed-off-by: Sameer Vohra <svohra@pivotal.io>

--- ci/tasks/bosh-release.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e -x
-
-install bosh-cli/bosh-cli* /usr/local/bin/bosh
-
-cd config-server
-bosh create-release --force --tarball=./config-server-release.tgz --name config-server --version acceptance
