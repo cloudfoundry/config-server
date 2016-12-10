d380f4c13a0ad1e288eb8edc2cefcaefd20e64fe
Dennis Leon <dleon@pivotal.io>
2016-12-09 18:17:36 -0800

    Require config_server.admin scope
    [#135885195](https://www.pivotaltracker.com/story/show/135885195)
    
    Signed-off-by: Dmitriy Kalinin <dkalinin@pivotal.io>

--- server/token_validator_jwt.go
+++ server/token_validator_jwt.go
@@ -8,6 +8,10 @@ import (
 	"github.com/dgrijalva/jwt-go"
 )
 
+const (
+	expectedScope = "config_server.admin"
+)
+
 type JwtTokenValidator struct {
 	verificationKey *rsa.PublicKey
 }
@@ -26,13 +30,37 @@ func NewJwtTokenValidator(jwtVerificationKeyPath string) (JwtTokenValidator, err
 	return JwtTokenValidator{verificationKey: verificationKey}, nil
 }
 
-func (j JwtTokenValidator) Validate(token string) error {
-	_, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+func NewJWTTokenValidatorWithKey(verificationKey *rsa.PublicKey) JwtTokenValidator {
+	return JwtTokenValidator{verificationKey: verificationKey}
+}
+
+func (j JwtTokenValidator) Validate(tokenStr string) error {
+	token, err := jwt.Parse(tokenStr, func(t *jwt.Token) (interface{}, error) {
+		if !j.isValidSigningMethod(t) {
+			return nil, errors.Error("Invalid signing method")
+		}
 		return j.verificationKey, nil
 	})
-
 	if err != nil {
-		return errors.WrapError(err, "Failed to validate token")
+		return errors.WrapError(err, "Validating token")
 	}
-	return nil
+
+	scopes := token.Claims.(jwt.MapClaims)["scope"].([]interface{})
+
+	for _, el := range scopes {
+		if el == expectedScope {
+			return nil
+		}
+	}
+
+	return errors.Errorf("Missing required scope: %s", expectedScope)
+}
+
+func (JwtTokenValidator) isValidSigningMethod(token *jwt.Token) bool {
+	switch token.Method {
+	case jwt.SigningMethodRS256, jwt.SigningMethodRS384, jwt.SigningMethodRS512:
+		return true
+	default:
+		return false
+	}
 }
