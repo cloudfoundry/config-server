f398b7ca48f9f2041688026418e786873ed56579
Maya Rosecrance <mrosecrance@pivotal.io>
2019-06-06 16:06:18 -0700

    Move pipeline to bosh-director team
    [#166391315](https://www.pivotaltracker.com/story/show/166391315)

--- ci/configure.sh
+++ ci/configure.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-fly -t production set-pipeline -p config-server -c ci/pipeline.yml --load-vars-from <(lpass show -G "config-server concourse secrets" --notes)
+fly -t director set-pipeline -p config-server -c ci/pipeline.yml --load-vars-from <(lpass show -G "config-server concourse secrets" --notes)
