d754a45a74d7995a9a5abd7164ce685f573c92ce
Joseph Palermo <jpalermo@pivotal.io>
2021-06-25 16:21:58 -0700

    Update build paths for new versions of golang

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -7,10 +7,10 @@ export PATH=/usr/local/go/bin:$GOPATH/bin:$PATH
 semver=`cat version-semver/number`
 filename="config-server-${semver}-${GOOS}-${GOARCH}"
 
-pushd github.com/cloudfoundry/config-server
+pushd config-server
   go build .
 popd
 
-mv config-server compiled-${GOOS}/${filename}
+mv config-server/config-server compiled-${GOOS}/${filename}
 
 openssl sha256 compiled-${GOOS}/${filename}
