05fea204b8dcfc35b32ed12900447a927a72d0e3
aram price <pricear@vmware.com>
2022-09-12 15:55:54 -0700

    Remove unused scripts
    linting consolidated to golangci-lint

--- bin/pre-commit-hooks.sh
+++ bin/pre-commit-hooks.sh
@@ -5,13 +5,7 @@ staged_files=$(git diff --cached --name-only --diff-filter=ACMR | grep '.go$')
 
 success=0
 
-./bin/check_gofmt "${staged_files}"
-[ $? -ne 0 ] && success=1
-
-./bin/check_golint "${staged_files}"
-[ $? -ne 0 ] && success=1
-
-./bin/govet
+./bin/lint "${staged_files}"
 [ $? -ne 0 ] && success=1
 
 exit ${success}
