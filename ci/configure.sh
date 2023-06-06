949a548ef97a1a31093caec04f48f21c5253d7c8
Long Nguyen <nguyenlo@vmware.com>
2023-06-06 14:37:45 -0400

    Updating to assume role arn

--- ci/configure.sh
+++ ci/configure.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-fly -t bosh-ecosystem set-pipeline -p config-server -c ci/pipeline.yml --load-vars-from <(lpass show -G "config-server concourse secrets" --notes)
+fly -t bosh-ecosystem set-pipeline -p config-server -c ci/pipeline.yml
