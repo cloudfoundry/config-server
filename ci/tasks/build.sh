7ad3af623bc96f65c9b807fda6bfaf86bef86745
aram price <aram.price@broadcom.com>
2026-07-01 14:42:47 -0700

    ci: fix build script

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -6,7 +6,7 @@ export PATH=/usr/local/go/bin:${PATH}
 binary_name="config-server-${GOOS}-${GOARCH}"
 
 pushd config-server
-  go build -o "${binary_name} ".
+  go build -o "${binary_name}" .
 
   openssl sha256 "${binary_name}"
 popd
